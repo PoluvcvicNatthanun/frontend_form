5b9f868318563610474335f663d32bb511380be3
MoveMaNow <nut647864@gmail.com>
2026-07-15 16:01:00 +0700

    update add function register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,13 +1,16 @@
 "use client";
 
+import React from "react";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
-export default function Register() {
+
+export default function register() {
   const [form, setForm] = useState({
-    txt_firstname: "",
-    txt_lastname: "",
-    txt_username: "",
-    txt_password: "",
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
   });
 
   const handleChange = (e) => {
@@ -19,9 +22,62 @@ export default function Register() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(form);
-  };
+    // console.log(form);
+    try {
+      const response = await fetch("https://api.itdev.cmtc.ac.th/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": 'application/json'
+        },
+        body: JSON.stringify({
+            firstname: form.txt_firstname,
+            lastname: form.txt_lastname,
+            sername: form.txt_username,
+            password: form.txt_password
+          }),
+      });
 
+      const result = await response.json();
+
+      if (response.ok) {
+        await Swal.fire({
+          icon: "success",
+          title: 'สมัครสมาชิกสำเร็จ(status: ${response.status})',
+          text: `เพิ่มผู้ใช้งานเรียบร้อยแล้ว`,
+          confirmButtonText: 'ตกลง',
+          confirmButtonColor: '#3085d6',
+        });
+
+      } else if (response.status === 400) {
+        await Swal.fire({
+          icon: "warning",
+          title: 'ข้อมูลไม่ถูกต้อง (status: ${response.status})',
+          text: result.message || "เกิดข้อผิดพลาด",
+          confirmButtonText: 'ตกลง',
+          confirmButtonColor: '#fff700',
+        });
+      } else if (response.status === 500) {
+        await Swal.fire({
+          icon: "error",
+          title: 'เกิดข้อผิดพลาดภายในเซิร์ฟเวอร์ (status: ${response.status})',
+          text: result.message || "เกิดข้อผิดพลาด",
+          confirmButtonText: 'ตกลง',
+          confirmButtonColor: '#ff0000',
+        });
+      }  
+      
+      
+      } catch (error) {
+        console.error("เกิดข้อผิดพลาด:", error);
+        await Swal.fire({
+          icon: "warning",
+          title: 'ไม่สามารถเชื่อมต่อกับเซิร์ฟเวอร์ได้',
+          text: 'กรุณาตรวจสอบการเชื่อมต่ออินเทอร์เน็ตของคุณและลองใหม่อีกครั้ง',
+          confirmButtonText: 'ตกลง',
+          confirmButtonColor: '#ff0000',
+        });
+      }
+    }
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-600 via-indigo-600 to-purple-700 flex items-center justify-center px-4 py-10">
       <div className="w-full max-w-2xl bg-white/95 backdrop-blur-md rounded-3xl shadow-2xl overflow-hidden">
@@ -46,7 +102,7 @@ export default function Register() {
             <input
               type="text"
               name="txt_firstname"
-              value={form.txt_firstname}
+              defaultValue=""
               onChange={handleChange}
               placeholder="กรอกชื่อ"
               className="w-full px-4 py-3 border rounded-xl text-gray-800 focus:outline-none focus:ring-4 focus:ring-blue-200 transition"
@@ -60,7 +116,7 @@ export default function Register() {
             <input
               type="text"
               name="txt_lastname"
-              value={form.txt_lastname}
+              defaultValue=""
               onChange={handleChange}
               placeholder="กรอกนามสกุล"
               className="w-full px-4 py-3 border rounded-xl text-gray-800 focus:outline-none focus:ring-4 focus:ring-blue-200 transition"
@@ -74,7 +130,7 @@ export default function Register() {
             <input
               type="text"
               name="txt_username"
-              value={form.txt_username}
+              defaultValue=""
               onChange={handleChange}
               placeholder="กรอก Username"
               className="w-full px-4 py-3 border rounded-xl text-gray-800 focus:outline-none focus:ring-4 focus:ring-blue-200 transition"
@@ -88,7 +144,7 @@ export default function Register() {
             <input
               type="password"
               name="txt_password"
-              value={form.txt_password}
+              defaultValue=""
               onChange={handleChange}
               placeholder="กรอก Password"
               className="w-full px-4 py-3 border rounded-xl text-gray-800 focus:outline-none focus:ring-4 focus:ring-blue-200 transition"
